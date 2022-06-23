ea15558ecafcf312659e5b6d3e5e11483b0c1cfa
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-23 15:53:06 +0300

    feat: create post-Adds templates, views and urls for creating post/

--- malombacodes_app/urls.py
+++ malombacodes_app/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns=[
     path('', views.index, name='index'),
     path('setup-profile/', views.create_profile, name='create_profile'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('create-post/', views.create_post, name='create_post')
 ]
--- malombacodes_app/views.py
+++ malombacodes_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
-from malombacodes_app.forms import Profile_Form
+from malombacodes_app.forms import Post_Form, Profile_Form
+from malombacodes_app.models import malombacodes_Post, malombacodes_Profile
 
 # Create your views here.
 
@@ -26,5 +27,25 @@ def create_profile(request):
 
 # @login_required(login_url='/accounts/login/')
 def profile(request):
+    if malombacodes_Profile.objects.filter(user_id=request.user.id).exists():
+        profile = malombacodes_Profile.objects.get(user_id=request.user.id)
+        posts = malombacodes_Post.objects.filter(user_id = request.user.id).all()
+        
+    else:
+        profile = None
+        posts= None
     return render(request, 'profile.html')
+
+def create_post(request):
+    if request.method == 'POST':
+        form = Post_Form(request.POST, request.FILES)
+        if form.is_valid():
+            post=form.save(commit = False)
+            post.user=request.user
+            post.save()
+            return redirect('profile')
+    else:
+        form=Post_Form()
+    return render(request, 'add/create_post.html', {'form':form})
+    
 
