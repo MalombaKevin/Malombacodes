7c4361d1503f02ccf67a7140077c914c3bf2bcef
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-23 14:39:16 +0300

    Perf: Create Profile Views - Adds view function to render the create profile form to the template

--- malombacodes_app/views.py
+++ malombacodes_app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
+from malombacodes_app.forms import Profile_Form
+
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+
+
+# @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
+
+
+# @login_required(login_url='/accounts/login/')
+def create_profile(request):
+    if request.method == 'POST':
+        form = Profile_Form(request.POST, request.FILES)
+        if form.is_valid():
+            profile=form.save(commit = False)
+            profile.user=request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form=Profile_Form()
+    return render(request, 'create_profile.html')
